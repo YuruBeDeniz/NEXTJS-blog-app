2dc1cfc02e87f93b64adaafc01eb375281411bdd
Deniz Saganak <saganak.deniz@gmail.com>
2023-02-11 16:03:34 +0100

    learn nextJS

--- pages/_app.js
+++ pages/_app.js
@@ -4,4 +4,9 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
+
+
+
+//In Next.js, you can add global CSS files by importing them from pages/_app.js.
+//You cannot import global CSS anywhere else.
